391b0f8218bc7d3613d8b3f7d3a057967b2c8268
高禄 <lu.gao@partner.midea.com>
2017-01-04 18:01:04 +0800

    gulp 热加载

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
-/**
- * Created by gao on 2016/12/30.
- */
-
 var gulp = require('gulp'),
-    less = require('gulp-less');
-
-//定义一个testLess任务（自定义任务名称）
-gulp.task('testLess', function () {
-    gulp.src('less/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('dest/'));
+  connect = require('gulp-connect'),
+  less = require('gulp-less');
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
 });
+ 
+gulp.task('html', function () { 
+  gulp.src('*.html')
+    .pipe(connect.reload());
+});
+gulp.task('watch', function () {
+  gulp.watch(['*.html'], ['html']);
+});
+gulp.task('less',function(){
+    gulp.src('./css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('css/'))
+        .pipe(connect.reload());
+        
+})
+gulp.task('watchLess',function(){
+    gulp.watch(['./css/*.less'],['less'],['html']);
+})
+gulp.task('default', ['connect', 'watch','watchLess']);
 
-gulp.task('default',['testLess']);
